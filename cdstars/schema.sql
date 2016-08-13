7d3ebc6f5343fe423e6ee5ceec4deaed3b52ab35
Tulonsae <admin@tulonsae.org>
2016-08-13 15:01:50 -0700

    Revised schema to match the acutal data (saving some db space).

--- cdstars/schema.sql
+++ cdstars/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `cdstars` (
-  `Name` varchar(32) NOT NULL,
+  `Name` varchar(26) NOT NULL,
   `Meaning` varchar(65) DEFAULT NULL,
-  `AltNames` varchar(128) DEFAULT NULL,
-  `Desg` varchar(32) NOT NULL,
+  `AltNames` varchar(121) DEFAULT NULL,
+  `Desg` varchar(25) NOT NULL,
   `HR` int(10) unsigned NOT NULL,
   `HD` int(10) unsigned NOT NULL,
-  `RA` varchar(10) NOT NULL,
-  `Decl` varchar(9) NOT NULL,
+  `RA` char(10) NOT NULL,
+  `Decl` char(9) NOT NULL,
   `V` float NOT NULL,
   `BV` float DEFAULT NULL,
-  `Spectrum` varchar(24) NOT NULL,
-  `Comments` varchar(720) DEFAULT NULL,
+  `Spectrum` varchar(17) NOT NULL,
+  `Comments` varchar(705) DEFAULT NULL,
   PRIMARY KEY (`HR`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
